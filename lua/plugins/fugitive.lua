58b55fd25aeabba921e5ade172f738d914ba22d0
nyx-void <amrit44404@proton.me>
2024-11-18 09:11:41 +0545

    more formatted

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -1,6 +1,6 @@
 return {
   "tpope/vim-fugitive",
-    config = function()
-      vim.keymap.set("n", "<leader>gs", ":Git<CR>", { desc = "Git status with Fugitive" })
-    end,
+  config = function()
+    vim.keymap.set("n", "<leader>gs", ":Git<CR>", { desc = "Git status with Fugitive" })
+  end,
 }
