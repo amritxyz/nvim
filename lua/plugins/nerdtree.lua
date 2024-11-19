5d8507bcaf365e709c1056cfc372cd6dc417b872
nyx-void <amrit44404@proton.me>
2024-11-20 02:11:59 +0545

    removed a plugin

--- lua/plugins/nerdtree.lua
+++
@@ -1,7 +0,0 @@
-return {
-  "preservim/nerdtree",
-  config = function()
-    vim.keymap.set("n", "<leader>n", ":NERDTreeToggle<CR>", { desc = "Toggle NerdTree"})
-    vim.cmd('autocmd BufEnter * if (winnr("$") == 1 && exists("b:NERDTree") && b:NERDTree.isTabTree()) | q | endif')
-  end
-}
