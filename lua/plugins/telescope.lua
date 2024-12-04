e10d3c3f0edc72d878330c0a381b6e03c5194f41
NyxVoid <amrit44404@proton.me>
2024-12-05 00:00:19 +0545

    new minor changes

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -4,12 +4,26 @@ return {
 	dependencies = { "nvim-lua/plenary.nvim" },  -- Dependency for Telescope
 
 	config = function()
+		require('telescope').setup {
+			defaults = {
+				file_ignore_patterns = {
+					"node_modules", "dist", "yarn.lock", ".cache/", ".git/", ".mozilla/",
+					".local/share/", "share/", ".local/state/", "state/"
+					-- "build",
+				},
+			}
+		}
 		-- Key mappings for Telescope
 		local builtin = require('telescope.builtin')
 
 		-- Define key mappings
-		vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+		-- Keybinding for Telescope find_files with hidden files and no ignore
+		vim.keymap.set('n', '<leader>ff', function()
+			require('telescope.builtin').find_files({ hidden = true })
+		end, { noremap = true, silent = true })
+
 		vim.keymap.set('n', '<leader>fg', builtin.git_files, { desc = 'Telescope git files' })
+
 		vim.keymap.set('n', '<leader>fh', function()
 			builtin.grep_string({ search = vim.fn.input("Grep > ") })  -- Grep search prompt
 		end, { desc = 'Telescope grep string' })
