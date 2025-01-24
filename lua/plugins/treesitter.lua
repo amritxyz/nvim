847a45fc8fd84e6688b4b37f8f22dbf192c4db24
amritxyz <amrit44404@proton.me>
2025-01-24 20:34:07 +0545

    minor bug fixed

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -5,7 +5,7 @@ return {
 		config = function()
 			local config = require("nvim-treesitter.configs")
 			config.setup({
-				ensure_install = { 'c', 'lua', 'rust', 'javascript', 'go', 'java', 'html', 'css', 'python', },
+				ensure_installed = { 'c', 'lua', 'rust', 'javascript', 'go', 'java', 'html', 'css', 'python', 'bash', },
 				sync_install = false,
 				auto_install = false,
 				highlight = { enable = true },
