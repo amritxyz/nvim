4364b0fa9d4a5a583668142cc0863749fe01268d
susma33 <susmatandon23@gmail.com>
2025-02-05 13:32:20 +0545

    absolute path

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -4,7 +4,29 @@ return {
 		require('lualine').setup({
 			options = {
 				theme = 'gruvbox'
-			}
+			},
+			sections = {
+				lualine_a = { 'mode' },
+				lualine_b = { 'branch' },
+				lualine_c = {
+					{
+						'filename',
+						path = 3,
+						shorting_target = 40,
+					}
+				},
+				lualine_x = { 'encoding', 'fileformat', 'filetype' },
+				lualine_y = { 'progress' },
+				lualine_z = { 'location' },
+			},
+			inactive_sections = {
+				lualine_a = {},
+				lualine_b = {},
+				lualine_c = { 'filename' },
+				lualine_x = {},
+				lualine_y = {},
+				lualine_z = {},
+			},
 		})
 	end
 }
